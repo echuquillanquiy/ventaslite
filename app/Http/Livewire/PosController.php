470392221d32007be9165e9627d6cad488b97e0d
Eduardo <echuquillanquiy@gmail.com>
2021-08-03 14:45:40 -0500

    componente ventas pos

--- app/Http/Livewire/PosController.php
+++ app/Http/Livewire/PosController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Denomination;
 use Livewire\Component;
 
 class PosController extends Component
 {
-    public $total = 10, $itemsQuantity = 1,$cart =  [];
+    public $total = 10, $itemsQuantity = 1, $cart =  [], $denominations = [], $efectivo, $change;
 
     public function render()
     {
+        $this->denominations = Denomination::all();
         return view('livewire.pos.component')->extends('layouts.theme.app')->section('content');
     }
 }
