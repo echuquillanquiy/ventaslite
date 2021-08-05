28a56391cd67e5c90ed171bf875a7645bb063538
Eduardo <echuquillanquiy@gmail.com>
2021-08-05 18:00:13 -0500

    agregando funcionalidad scroll

--- app/Http/Livewire/PosController.php
+++ app/Http/Livewire/PosController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Livewire;
 
 use App\Models\Denomination;
+use App\Models\Product;
+use App\Models\Sale;
+use App\Models\SaleDetail;
 use Darryldecode\Cart\Facades\CartFacade as Cart;
+use DB;
+use Illuminate\Support\Facades\Redirect;
 use Livewire\Component;
 
 class PosController extends Component
@@ -41,4 +46,207 @@ class PosController extends Component
         'saveSale' => 'saveSale'
     ];
 
+    public function ScanCode($barcode, $cant = 1)
+    {
+        $product = Product::where('barcode', $barcode)->first();
+
+        if ($product == null || empty($empty))
+        {
+            $this->emit('scan-notfound', 'El prodcuto no está registrado');
+        } else {
+            if ($this->InCart($product->id))
+            {
+                $this->increaseQty($product->id);
+                return;
+            }
+
+            if ($product->stock < 1)
+            {
+                $this->emit('no-stock', 'Stock insuficiente :/');
+                return;
+            }
+
+            Cart::add($product->id, $product->name, $product->price, $cant, $product->image);
+            $this->total = Cart::getTotal();
+
+            $this->emit('scan-ok', 'Producto agregado');
+        }
+    }
+
+    public function InCart($productId)
+    {
+        $exist = Cart::get($productId);
+        if ($exist)
+            return true;
+        else
+            return false;
+    }
+
+    public function increaseQty($productId, $cant = 1)
+    {
+        $title = '';
+        $product = Product::find($productId);
+        $exist = Cart::get($productId);
+
+        if ($exist)
+            $title = 'Cantidad actualizada';
+        else
+            $title = 'Producto agregado';
+
+        if ($exist)
+        {
+            if ($product->stock < ($cant + $exist->quantity))
+            {
+                $this->emit('no-stock', 'Stock insuficiente :/');
+                return;
+            }
+        }
+
+        Cart::add($product->id, $product->name, $product->price, $cant, $product->image);
+
+        $this->total = Cart::getTotal();
+        $this->itemsQuantity = Cart::getTotalQuantity();
+
+        $this->emit('scan-ok', $title);
+    }
+
+    public function updateQuantity($productId, $cant = 1)
+    {
+        $title = '';
+        $product = Product::find($productId);
+        $exist = Cart::get($productId);
+
+        if ($exist)
+            $title = 'Cantidad actualizada';
+        else
+            $title = 'Producto agregado';
+
+        if ($exist)
+        {
+            if ($product->stock < $cant)
+            {
+                $this->emit('no-stock', 'Stock insuficiente :/');
+                return;
+            }
+        }
+
+        $this->removeItem($productId);
+
+        if ($cant > 0)
+        {
+            Cart::add($product->id, $product->name, $product->price, $cant, $product->image);
+            $this->total = Cart::getTotal();
+            $this->itemsQuantity = Cart::getTotalQuantity();
+
+            $this->emit('scan-ok', $title);
+        }
+    }
+
+    public function removeItem($productId)
+    {
+        Cart::remove($productId);
+        $this->total = Cart::getTotal();
+        $this->itemsQuantity = Cart::getTotalQuantity();
+
+        $this->emit('scan-ok', 'producto eliminado');
+    }
+
+    public function decreaseQty($productId)
+    {
+        $item = Cart::get($productId);
+        Cart::remove($productId);
+
+        $newQty = ($item->quantity) - 1;
+        if ($newQty > 0)
+            Cart::add($item->id, $item->name, $item->price, $newQty, $item->attributes[0]);
+
+        $this->total = Cart::getTotal();
+        $this->itemsQuantity = Cart::getTotalQuantity();
+
+        $this->emit('scan-ok', 'Cantidad actualizada');
+
+    }
+
+    public function clearCart()
+    {
+        Cart::clear();
+        $this->efectivo = 0;
+        $this->change = 0;
+
+        $this->total = Cart::getTotal();
+        $this->itemsQuantity = Cart::getTotalQuantity();
+
+        $this->emit('scan-ok', 'Carrito vacio');
+    }
+
+    public function saveSale()
+    {
+        if ($this->total <= 0)
+        {
+            $this->emit('sale-error','AGREGA PRODUCTOS A LA VENTA');
+            return;
+        }
+
+        if ($this->efectivo <= 0)
+        {
+            $this->emit('sale-error','INGRESA EL EFECTIVO');
+            return;
+        }
+        if ($this->total > $this->efectivo)
+        {
+            $this->emit('sale-error','EL EFECTIVO DEBE SER MAYOR O IGUAL AL TOTAL');
+            return;
+        }
+
+        DB::beginTransaction();
+
+            try {
+                $sale = Sale::create([
+                    'total' => $this->total,
+                    'items' => $this->itemsQuantity,
+                    'cash' => $this->efectivo,
+                    'change' => $this->change,
+                    'user_id' => auth()->user()->id,
+                ]);
+
+                if ($sale)
+                {
+                    $items = Cart::getContent();
+
+                    foreach ($items as $item)
+                    {
+                        SaleDetail::create([
+                            'price' => $item->price,
+                            'quantity' => $item->quantity,
+                            'product_id' => $item->id,
+                            'sale_id' => $sale->id,
+                        ]);
+
+                        $product = Product::find($item->id);
+                        $product->stock = $product->stock - $item->quantity;
+                        $product->save();
+                    }
+                }
+
+            DB::commit();
+
+                Cart::clear();
+
+                $this->efectivo = 0;
+                $this->change = 0;
+                $this->total = Cart::getTotal();
+                $this->itemsQuantity = Cart::getTotalQuantity();
+                $this->emit('sale-ok', 'Venta registrada con éxito');
+                $this->emit('print-ticket', $sale->id);
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+                $this->emit('sale-error', $e->getMessage());
+        }
+    }
+
+    public function printTicket($sale)
+    {
+        return Redirect::to("print://$sale->id");
+    }
 }
---
+++ resources/views/livewire/pos/scripts/general.blade.php
@@ -0,0 +1,8 @@
+<script>
+    $(".do-nicescrol").niceScroll({
+        cursorcolor:"aquamarine",
+        cursorwidth:"24px",
+        background:"rgba(20,20,20,0.3)",
+        cursorborder:"1px solid aquamarine",
+        cursorborderradius:0
+</script>
